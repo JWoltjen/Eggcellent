c17a93f21dd971d752e5059c743ec7c61f844fd9
Jeff Woltjen <jeff.woltjen@gmail.com>
2021-05-03 15:56:33 -0500

    add links to navbar menu component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,13 @@ import { Link } from 'react-router-dom'
 export default function Navbar() {
     return (
        <nav>
-           <Link to="/">EGG</Link>
+           <Link to="/" className='pl-8'>EGG</Link>
+           <div className='px-4 cursor-pointer md:hidden'>
+               <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" /></svg>
+           </div>
+           <div className='pr-8 md:block hidden'>
+               
+           </div>
        </nav>
     )
 }

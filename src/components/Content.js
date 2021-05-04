47e7374a48bfaa12230093d5e49f8a249cf918bc
Jeff Woltjen <jeff.woltjen@gmail.com>
2021-05-04 09:48:56 -0500

    store menu-card class in index.css

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,15 +1,27 @@
 import React from 'react'
-import ImageOne from '../images/egg1.jpg'
-import ImageTwo from '../images/egg2.jpg'
+import ImageOne from '../images/egg1.jpeg'
+import ImageTwo from '../images/egg2.jpeg'
 
 function Content() {
     return (
-        <div className='flex flex-col justify-center items-center bg-white h-screen font-mono py-40'>
-            <img src={ImageOne} alt="egg" className='h-full rounded mb-20 shadow'/>
-            <div className='flex flex-col justify-center items-center'>
-                <h2 className='text-2xl mb-2'>Egg Muffins</h2>
+        <>
+            <div className='menu-card'>
+                <img src={ImageOne} alt="egg" className='h-full rounded mb-20 shadow'/>
+                <div className='flex flex-col justify-center items-center'>
+                    <h2 className='text-2xl mb-2'>Egg Muffins</h2>
+                    <p className='mb-2'>I like eggs</p>
+                    <span>$80</span>
+                </div>
             </div>
-        </div>
+            <div className='flex flex-col justify-center items-center bg-white h-screen font-mono py-40'>
+                <img src={ImageTwo} alt="egg" className='h-full rounded mb-20 shadow'/>
+                <div className='flex flex-col justify-center items-center'>
+                    <h2 className='text-2xl mb-2'>Egg Salad</h2>
+                    <p className='mb-2'>Who doesn't like eggs?</p>
+                    <span>$40</span>
+                </div>
+            </div>
+        </>
     )
 }
 

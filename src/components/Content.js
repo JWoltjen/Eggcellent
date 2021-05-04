7ec253434adee40949e101d4d30f69b631b47ff7
Jeff Woltjen <jeff.woltjen@gmail.com>
2021-05-04 09:51:44 -0500

    add center-content class in index.css :

--- src/components/Content.js
+++ src/components/Content.js
@@ -7,15 +7,15 @@ function Content() {
         <>
             <div className='menu-card'>
                 <img src={ImageOne} alt="egg" className='h-full rounded mb-20 shadow'/>
-                <div className='flex flex-col justify-center items-center'>
+                <div className='center-content'>
                     <h2 className='text-2xl mb-2'>Egg Muffins</h2>
                     <p className='mb-2'>I like eggs</p>
                     <span>$80</span>
                 </div>
             </div>
-            <div className='flex flex-col justify-center items-center bg-white h-screen font-mono py-40'>
+            <div className='menu-card'>
                 <img src={ImageTwo} alt="egg" className='h-full rounded mb-20 shadow'/>
-                <div className='flex flex-col justify-center items-center'>
+                <div className='center-content'>
                     <h2 className='text-2xl mb-2'>Egg Salad</h2>
                     <p className='mb-2'>Who doesn't like eggs?</p>
                     <span>$40</span>
